6af8d177f387f8768f16dca21bbd8c08addec4a9
Helana Maliakal <maliakaa@tcd.ie>
2019-10-18 12:29:43 +0100

    added tests

--- LowestCommonAncestor.py
+++ LowestCommonAncestor.py
@@ -10,25 +10,25 @@ class lca:
         if_acyclic = networkx.is_directed_acyclic_graph(graph)
         return if_acyclic
 
-if __name__ == "__main__":
-    lca = lca()
+# if __name__ == "__main__":
+#     lca = lca()
 
-    G = networkx.DiGraph()
-    map(G.add_node, range(1, 8))
-    G.add_edge(1, 2)
-    G.add_edge(1, 3)
-    G.add_edge(2, 4)
-    G.add_edge(2, 5)
-    G.add_edge(3, 6)
-    G.add_edge(4, 7)
-    G.add_edge(5, 7)
-    G.add_edge(6, 5)
-    G.add_edge(6, 7)
+#     G = networkx.DiGraph()
+#     map(G.add_node, range(1, 8))
+#     G.add_edge(1, 2)
+#     G.add_edge(1, 3)
+#     G.add_edge(2, 4)
+#     G.add_edge(2, 5)
+#     G.add_edge(3, 6)
+#     G.add_edge(4, 7)
+#     G.add_edge(5, 7)
+#     G.add_edge(6, 5)
+#     G.add_edge(6, 7)
 
-    lca_a = lca.do_lca(G, 4, 6)
-    if_acyclic = lca.check_acyclic(G)
-    print(if_acyclic)
-    print(lca_a)
+#     lca_a = lca.do_lca(G, 4, 6)
+#     if_acyclic = lca.check_acyclic(G)
+#     print(if_acyclic)
+#     print(lca_a)
 
 
 
--- test_LCA.py
+++ test_LCA.py
@@ -1,13 +1,42 @@
 import unittest
-from LowestCommonAncestor import Node
+import networkx
 from LowestCommonAncestor import lca
 
 class TestLCA(unittest.TestCase):
 
     def test_find_lca(self):
-        self.assertEqual(lca.find_lca(fdfd), 1)
+        lca_test = lca()
 
+        G = networkx.DiGraph()
+        map(G.add_node, range(1, 8))
+        G.add_edge(1, 2)
+        G.add_edge(1, 3)
+        G.add_edge(2, 4)
+        G.add_edge(2, 5)
+        G.add_edge(3, 6)
+        G.add_edge(4, 7)
+        G.add_edge(5, 7)
+        G.add_edge(6, 5)
+        G.add_edge(6, 7)
+         
+        self.assertEqual(lca_test.do_lca(G, 4, 6), 1)
 
+    def test_check_acyclic(self):
+        lca_test = lca()
+
+        G = networkx.DiGraph()
+        map(G.add_node, range(1, 8))
+        G.add_edge(1, 2)
+        G.add_edge(1, 3)
+        G.add_edge(2, 4)
+        G.add_edge(2, 5)
+        G.add_edge(3, 6)
+        G.add_edge(4, 7)
+        G.add_edge(5, 7)
+        G.add_edge(6, 5)
+        G.add_edge(6, 7)
+         
+        self.assertEqual(lca_test.check_acyclic(G), True)
 
 if __name__ == '__main__':
     unittest.main()
